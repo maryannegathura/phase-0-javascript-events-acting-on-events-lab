0d2340d4c5af255dcd16eff76e0c824974572371
maryannegathura <mariannegathura7@gmail.com>
2024-07-22 17:48:57 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Your code here
+// helpers.js
+
+function moveDodgerLeft() {
+    const dodger = document.getElementById('dodger');
+    let leftPosition = parseInt(dodger.style.left) || 0;
+    leftPosition -= 1;
+    dodger.style.left = `${leftPosition}px`;
+  }
+  
+  function moveDodgerRight() {
+    const dodger = document.getElementById('dodger');
+    let leftPosition = parseInt(dodger.style.left) || 0;
+    leftPosition += 1;
+    dodger.style.left = `${leftPosition}px`;
+  }
+  
+  module.exports = {
+    moveDodgerLeft,
+    moveDodgerRight
+  };
+  
